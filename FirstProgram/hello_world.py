33abbd2e1b6e14046b8424ce6613d5f9b0d8ec79
Rashi Aswani <rashiaswani2509@gmail.com>
2024-02-16 18:41:12 +0530

    First Commit

---
+++ FirstProgram/hello_world.py
@@ -0,0 +1,28 @@
+# Pseudo-code for hello_world.py
+
+# Prompt the user to enter their name using input()
+# Read the user's name using input()
+# Print out the user's name
+
+# Prompt the user to enter their age using input()
+# Read the user's age using input()
+# Print out the user's age
+
+# Print the string "Hello World!" on a new line
+
+# Python code for hello_world.py
+
+# Prompt the user to enter their name using input()
+user_name = input("Enter your name: ")
+
+# Print out the user's name
+print("Your name is:", user_name)
+
+# Prompt the user to enter their age using input()
+user_age = input("Enter your age: ")
+
+# Print out the user's age
+print("Your age is:", user_age)
+
+# Print the string "Hello World!" on a new line
+print("Hello World!")
